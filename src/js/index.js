6226ea4a888c95253dd1d026f4f1df5b7af8e07f
Gabriel Merêncio <gmerencio.santos@gmail.com>
2019-10-07 20:28:42 -0300

    Added JS functionality to dynamically change the prices

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,18 @@
+import { PRICING_OPTIONS, fillPrices } from './prices';
 import 'normalize.css';
 import './../scss/main.scss';
+
+window.onload = function() {
+  // Fill in prices and add event listeners for changes in pricing option.
+  
+  fillPrices(PRICING_OPTIONS.MONTHLY);
+  
+  var checkBox = document.querySelector(".js-pricing-checkbox");
+  checkBox.addEventListener("click", function(event) {
+    if(checkBox.checked) {
+      fillPrices(PRICING_OPTIONS.YEARLY);
+    } else {
+      fillPrices(PRICING_OPTIONS.MONTHLY);
+    }
+  });
+}
---
+++ src/js/prices.js
@@ -0,0 +1,22 @@
+var MONTH_PRICES = ["00.0", "9.99", "14.99"];
+var YEARLY_PRICES = ["00.0", "99.99", "154.99"];
+export var PRICING_OPTIONS = {MONTHLY: "month", YEARLY: "year"};
+
+/**
+ * Fills in the prices according to the argument, which is one of the
+ * PRICING_OPTIONS objects.
+ */
+
+export var fillPrices = function(opt) {
+  var prices = document.querySelectorAll(".js-price");
+  
+  for(var i = 0; i < prices.length; i++) {
+    if(opt === PRICING_OPTIONS.MONTHLY) {
+      prices[i].innerHTML = "$ " + MONTH_PRICES[i];
+    }
+    
+    else if(opt === PRICING_OPTIONS.YEARLY) {
+      prices[i].innerHTML = "$ " + YEARLY_PRICES[i];
+    }
+  }
+}
